85389852d71a4daebe744cf17e52d3a92cfe40a7
Matthew Horwood <matt@horwood.biz>
2024-11-02 16:46:22 +0000

    undo that

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -19,11 +19,13 @@ const Link = styled.a`
 `;
 
 export const Footer = () => (
-  <footer id="myFooter">
-    <div class="w3-container w3-theme-l1 horwood-bottom">
-      <section class='copyright'>
-        <p class='copyright-info'>&copy; Copyright 2004 - 2024</p>
-      </section>
-    </div>
-  </footer>
+  <Box>
+    <Link
+      rel="noopener"
+      target="_blank"
+      href="https://github.com/mhzawadi/issue-status"
+    >
+      Powered by Issue Status
+    </Link>
+  </Box>
 );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,19 +4,4 @@ const Title = styled.h1`
   color: ${(props) => props.theme.colors.text};
 `;
 
-export const Header = () => (
-    <div class="w3-top">
-      <div class="w3-bar w3-top w3-left-align w3-large horwood-top">
-        <a id="site_logo" href="{{ base_url }}" class="w3-bar-item w3-button w3-theme-l1 horwood-logo">
-          <img src="https://owncloud.horwood.biz/apps/theming/image/logo?v=25" alt="Site Logo" height="42" width="49">
-        </a>
-      </div>
-      <div class="w3-bar w3-top w3-left-align w3-large horwood-bcs" style="top: 66px;">
-        <span class='seven-col'>
-        </span>
-        <span class="horwood-temp"></span>
-        <div class='five-col search noprint' role='search'>
-        </div>
-      </div>
-    </div>
-    );
+export const Header = () => <Title>💗 Issue Status</Title>;
