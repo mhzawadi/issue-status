97f4ac75064cc7248eae196fb9bcf2cfad6def1c
Matthew Horwood <matt@horwood.biz>
2025-06-22 09:59:30 +0100

    only build when needed

--- issue-status.config.ts
+++ issue-status.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "issue-status";
 import { github } from "issue-status/providers";
 
 export default defineConfig({
-  name: "Horwood Status",
+  name: "Horwood System Status",
   description: "Status page for my services",
   provider: github({
     owner: "mhzawadi",
